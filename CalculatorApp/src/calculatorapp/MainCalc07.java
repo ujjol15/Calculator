d7dcf4b92adde9f92a482d5605c41a87eb838bf4
ujjol15 <ujjolbd14@gmail.com>
2015-08-22 17:55:49 +0600

    Calculator
    Ujjol Ahmed Anik

---
+++ CalculatorApp/src/calculatorapp/MainCalc07.java
@@ -0,0 +1,19 @@
+
+
+package calculatorapp;
+
+import javax.swing.JFrame; 
+
+
+public class MainCalc07 {
+     public static void main(String []args)
+    {
+        Calc07 ob=new Calc07();
+       ob.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+ob.setSize( 560, 360 ); 
+ ob.setVisible( true ); 
+    
+    
+    
+    }
+}
